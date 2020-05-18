9573ffc0c9c7fa87c300c4ca51497dc04dc0559f
Matthew Fritsch <matthewmfritsch@gmail.com>
2020-05-17 23:38:48 -0700

    Added modules to a single folder

--- bot.py
+++ bot.py
@@ -1,8 +1,13 @@
 from discord.ext import commands
-from eventmanager import add_event
+from scripts.eventmanager import add_event, _myEvents
 
 client = commands.Bot(command_prefix='!sch ')
 client.remove_command('help')
+commandList = ['add', 'events', 'help']
+
+
+def pingUser(ctx):
+    await ctx.send('<@' + str(ctx.author.id) + '>')
 
 
 @client.event
@@ -12,9 +17,8 @@ async def on_ready():
 
 @client.command()
 async def add(ctx, *, message):
-    print(ctx, message)
-    success = add_event(message)
-    await ctx.send(success)
+    pingUser(ctx)
+    await ctx.send(add_event(message))
 
 
 @client.event
@@ -28,17 +32,24 @@ async def on_command_error(ctx, error):
 
 @client.command()
 async def help(ctx, message):
-    await ctx.send('<@'+str(ctx.author.id)+'>')
+    if message is None:
+        await ctx.send('Type !sch <command> [OPTIONS], or !sch help <command> to see correct syntax!')
+        cmdPrint = 'Commands include:'
+        for cmd in commandList:
+            cmdPrint += '\n- ' + cmd
+        await ctx.send(cmdPrint)
     if message == "add":
         await ctx.send('!sch add <eventName> [OPTIONS]\nOptions:\n -d <date>\n -t <time>\n -s <summary>\n -r <role>')
         await ctx.send('> !sch add Essay -d 5-22 -t 11PM -r @essayTeam')
+        return
     if message == "events":
         await ctx.send('!sch events\nThis command sends the list of all events stored by @scheduleBot')
+        return
 
 
 @client.command()
 async def events(ctx):
-    _myEvents = 0
+    pingUser(ctx)
     for e in _myEvents:
         await ctx.send(e)
     else:
