290838360b6bb395d35e2c36d57d9d8ab757d3bc
Matthew Fritsch <matthewmfritsch@gmail.com>
2020-05-19 10:39:49 -0700

    replaced date and time with date_time

--- scripts/events.py
+++ scripts/events.py
@@ -1,17 +1,14 @@
 class Event:
-    def __init__(self, name, date, time, summ, role):
+    def __init__(self, name, date_time, summ, role):
         self.name = name
-        self.date = date
-        self.time = time
+        self.date_time = date_time
         self.role = role
         self.summ = summ
 
     def __repr__(self):
         toRet = "Event: \n--Name: " + self.name
-        if self.date is not None:
+        if self.date_time is not None:
             toRet = toRet + "\n--Date: " + self.date
-        if self.time is not None:
-            toRet = toRet + "\n--Time: " + self.time
         if self.summ is not None:
             toRet = toRet + "\n--Summ: " + self.summ
         if self.role is not None:
@@ -21,11 +18,8 @@ class Event:
     def get_name(self):
         return self.name
 
-    def get_date(self):
-        return self.date
-
-    def get_time(self):
-        return self.time
+    def get_date_time(self):
+        return self.date_time
 
     def get_role(self):
         return self.role
@@ -35,13 +29,10 @@ class Event:
 
     def set_name(self, name):
         self.name = name
-    
-    def set_date(self, date):
-        self.date = date
-    
-    def set_time(self, time):
-        self.time = time
-    
+
+    def set_date_time(self, date_time):
+        self.date_time = date_time
+
     def set_role(self, role):
         self.role = role
  
